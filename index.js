ff511c5ea19f4389d7bd2283f806d81bc9ad4616
Ashutosh Kumar <raj.fungus@gmail.com>
2021-06-16 20:34:28 +0530

    json

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ mongoose.connect(process.env.MONGODB_URI, {
 
 })
 app.use(express.urlencoded({ extended: false }));
+app.use(express.json())
 app.use(ddos.express)
 app.set('view engine', 'ejs')
 app.use(express.static(__dirname + '/public'))
