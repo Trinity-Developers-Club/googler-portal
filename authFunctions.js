17ec5bb5c3761faf5c1fca3b550228eaf56f36f2
“Ashutosh <ashutosh.kumar@forty4hz.com>
2021-11-20 12:23:45 +0530

    TIMESET

--- authFunctions.js
+++ authFunctions.js
@@ -14,8 +14,8 @@ function checkUnAuthenticated(req, res, next) {
 }
 
 function checkEventTime(req, res, next) {
-    let startTime = new Date(Date.parse('20 November 2021 06:30'))
-    let closeTime = new Date(Date.parse('20 November 2021 07:00'))
+    let startTime = new Date(Date.parse('20 November 2021 12:30'))
+    let closeTime = new Date(Date.parse('20 November 2021 01:00'))
     console.log(startTime)
     let currentDate = new Date();
     if (currentDate < startTime) {
